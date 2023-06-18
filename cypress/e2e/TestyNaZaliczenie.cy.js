b1b31ec8fee33247ab3892072a05ec95ebbff798
Łukasz Rogowski <rogowski.poczta@gmail.com>
2023-06-18 21:23:06 +0200

    48

--- cypress/e2e/TestyNaZaliczenie.cy.js
+++ cypress/e2e/TestyNaZaliczenie.cy.js
@@ -5,7 +5,7 @@
 describe.only('Navigate to Google.pl and assert', () => {
 
     beforeEach("Setup", () => {
-        cy.visit('https://www.google.pl/')
+        cy.visit('/')
         cy.url().should("contain", "google")
     });
     
