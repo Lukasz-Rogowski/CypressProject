787e40b9cc192feec57d8c30780251feeaf7bcfc
Łukasz Rogowski <rogowski.poczta@gmail.com>
2023-06-18 16:42:39 +0200

    Piaty commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  videoUploadOnPasses: false,
   projectId: 'p3f9q7',
   reporter: "mochawesome",
   reporterOptions: {
